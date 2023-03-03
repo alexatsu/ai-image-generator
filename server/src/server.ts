935c78ddec8aed5d95718118608f2f3482688583
Alex <trevan98@gmail.com>
2023-03-03 23:23:08 +0300

    wip, backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,16 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
-})
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
